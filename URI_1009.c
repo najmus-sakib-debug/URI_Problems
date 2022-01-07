9e675e33f6df443b30f04c78c461acc90bba103f
_MUSAFIR_ <71838370+najmus-sakib-debug@users.noreply.github.com>
2022-01-07 20:22:40 +0600

    Add files via upload

---
+++ URI_1009.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char a[30];
+    double b,c,d;
+    gets(a);
+    scanf("%lf%lf", &b, &c);
+    d = b + (c * (15.0/100));
+    printf("TOTAL = R$ %.2lf\n", d);
+    return 0;
+}
